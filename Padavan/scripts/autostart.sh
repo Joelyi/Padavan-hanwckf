5e7622daa2098e31c6ada35b45bfca77b923f9b2
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-12-28 20:05:42 +0800

    Update autostart.sh

--- Padavan/scripts/autostart.sh
+++ Padavan/scripts/autostart.sh
@@ -17,11 +17,6 @@ logger -t "自动启动" "正在启动SmartDNS"
 /usr/bin/smartdns.sh start
 fi
 
-if [ $(nvram get caddy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动文件管理"
-/usr/bin/caddy.sh start
-fi
-
 logger -t "自动启动" "正在检查路由是否已连接互联网！"
 count=0
 while :
@@ -50,37 +45,3 @@ do
 	fi
 done
 
-if [ $(nvram get adbyby_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动adbyby plus+"
-/usr/bin/adbyby.sh start
-fi
-
-if [ $(nvram get koolproxy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动koolproxy"
-/usr/bin/koolproxy.sh start
-fi
-
-if [ $(nvram get aliddns_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动阿里ddns"
-/usr/bin/aliddns.sh start
-fi
-
-if [ $(nvram get ss_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动科学上网"
-/usr/bin/shadowsocks.sh start
-fi
-
-if [ $(nvram get adg_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动adguardhome"
-/usr/bin/adguardhome.sh start
-fi
-
-if [ $(nvram get wyy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动音乐解锁"
-/usr/bin/unblockmusic.sh start
-fi
-
-if [ $(nvram get zerotier_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动zerotier"
-/usr/bin/zerotier.sh start
-fi
